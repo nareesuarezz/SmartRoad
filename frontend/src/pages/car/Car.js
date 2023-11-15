cf487ed6ac47f1e06889f8da99d9e9d73debef07
nareesuarezz <naremehs11@gmail.com>
2023-11-15 11:02:56 +0000

    añadidas las pantallas de bicicleta y coche e implementado un sistema de notificacion (falta logica)

--- frontend/src/pages/car/Car.js
+++ frontend/src/pages/car/Car.js
@@ -1,9 +1,64 @@
-function Car(){
-    return(
+import React, { useState, useEffect } from 'react';
+import logoCar from '../../img/car.png';
+import './Car.css';
+import bikeWarningSound from '../../sounds/sound.mp3';
+import { ArrowLeftOutlined } from '@ant-design/icons';
+
+
+function Car() {
+    const [showModal, setShowModal] = useState(false);
+
+    useEffect(() => {
+        const notificationTimeout = setTimeout(() => {
+            setShowModal(true);
+            playWarningSound();
+
+            const hideNotificationTimeout = setTimeout(() => {
+                setShowModal(false);
+            }, 5000);
+
+            return () => clearTimeout(hideNotificationTimeout);
+        }, 5000);
+
+        return () => clearTimeout(notificationTimeout);
+    }, []);
+
+    const playWarningSound = () => {
+        const audio = new Audio(bikeWarningSound);
+        audio.play();
+    };
+
+    const goBack = () => {
+        window.location.href = "/home";
+    }
+    return (
         <>
-        ESTO ES CAR
+        <div className='arrow' onClick={() => goBack()}><ArrowLeftOutlined /></div>
+            <div className="car-title">
+                <h1>SmartRoad</h1>
+            </div>
+            <div className="logged">
+                <h2>You are now logged in as a...</h2>
+            </div>
+
+            <div className="car-container">
+                <div className="vehicle-box car-box">
+                    <img src={logoCar} alt="Car Logo" />
+                </div>
+                <p className='car'>Car</p>
+
+                <h3 className='warn'>Now you will be warned in case that a bicycle is near you.</h3>
+            </div>
+
+            {showModal && (
+                <div className="modal-overlay">
+                    <div className="modal">
+                        <p>WARNING: THERE IS A BIKE NEAR YOU, BE CAREFUL</p>
+                    </div>
+                </div>
+            )}
         </>
-    )
+    );
 }
 
 export default Car;
--- frontend/src/pages/home/Home.js
+++ frontend/src/pages/home/Home.js
@@ -4,24 +4,51 @@ import logoCar from '../../img/car.png';
 import './Home.css';
 
 function Home() {
-    const handleClick = () => {
-        debugger
+    const handleClick = (vehicle) => {
+        debugger;
         const x = document.getElementById("demo");
 
         function getLocation() {
-            if (navigator.geolocation) {
-                navigator.geolocation.getCurrentPosition(showPosition);
-            } else {
-                x.innerHTML = "Geolocation is not supported by this browser.";
-            }
+    if (navigator.geolocation) {
+        navigator.geolocation.watchPosition(
+            (position) => showPosition(position, vehicle),
+            showError,
+            { enableHighAccuracy: true }
+        );
+    } else {
+        x.innerHTML = "Geolocation is not supported by this browser.";
+    }
+}
 
-        }
 
-        function showPosition(position) {
+        function showPosition(position, vehicle) {
             x.innerHTML = "Latitude: " + position.coords.latitude +
                 "<br>Longitude: " + position.coords.longitude;
-                
+
+            if (vehicle === 'bicycle') {
+                window.location.href = "/bicycle";
+            } else if (vehicle === 'car') {
+                window.location.href = "/car";
+            }
         }
+
+        function showError(error) {
+            switch (error.code) {
+                case error.PERMISSION_DENIED:
+                    x.innerHTML = "User denied the request for Geolocation.";
+                    break;
+                case error.POSITION_UNAVAILABLE:
+                    x.innerHTML = "Location information is unavailable.";
+                    break;
+                case error.TIMEOUT:
+                    x.innerHTML = "The request to get user location timed out.";
+                    break;
+                case error.UNKNOWN_ERROR:
+                    x.innerHTML = "An unknown error occurred.";
+                    break;
+            }
+        }
+
         getLocation();
     }
 
@@ -35,11 +62,11 @@ function Home() {
             </div>
 
             <div className="vehicle-container">
-                <div className="vehicle-box bicycle-box" onClick={handleClick}>
+                <div className="vehicle-box bicycle-box" onClick={() => handleClick('bicycle')}>
                     <img src={logoBicycle} alt="Logo de bicicleta" />
                 </div>
                 <p className='bicycle'>Bicicleta</p>
-                <div className="vehicle-box car-box" onClick={handleClick}>
+                <div className="vehicle-box car-box" onClick={() => handleClick('car')}>
                     <img src={logoCar} alt="Logo de coche" />
                 </div>
                 <p className='car'>Coche</p>
