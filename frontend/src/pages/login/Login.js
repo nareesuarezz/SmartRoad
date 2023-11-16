8cdba261065dd4d5b8decf477f39f2e058edbf8b
= <=>
2023-11-16 16:41:25 +0000

    login acabado

--- frontend/src/pages/login/Login.js
+++ frontend/src/pages/login/Login.js
@@ -1,42 +1,71 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './Login.css'
+import React, { useState } from 'react';
+import './Login.css';
+import { ArrowLeftOutlined } from '@ant-design/icons';
 
-function Login() {
-  return (
-    <div className="container">
-      <div className="form-box">
-        <div className="header-form">
-          <h4 className="text-primary text-center"><i className="fa fa-user-circle" style={{ fontSize: "110px" }}></i></h4>
-          <div className="image">
-          </div>
-        </div>
-        <div className="body-form">
-          <form>
-            <div className="input-group mb-3">
-              <div className="input-group-prepend">
-                <span className="input-group-text"><i className="fa fa-user"></i></span>
-              </div>
-              <input type="text" className="form-control" placeholder="Username" />
-            </div>
-            <div className="input-group mb-3">
-              <div className="input-group-prepend">
-                <span className="input-group-text"><i className="fa fa-lock"></i></span>
-              </div>
-              <input type="text" className="form-control" placeholder="Password" />
-            </div>
-            <button type="button" className="btn btn-secondary btn-block">LOGIN</button>
-            <div className="message">
-              <div><input type="checkbox" /> Relembre-me</div>
-              <div><a href="#">Esqueci a senha</a></div>
-            </div>
-          </form>
-        </div>
-      </div>
-    </div>
-  );
+
+const Login = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [rememberMe, setRememberMe] = useState(false);
+
+  const handleLogin = () => {
+    console.log('Username:', username);
+    console.log('Password:', password);
+    console.log('Remember Me:', rememberMe);
+  };
+
+  const goBack = () => {
+    window.location.href = "/home";
 }
 
-export default Login;
+  return (
+    <> <div className='arrow' onClick={() => goBack()}><ArrowLeftOutlined /></div>
+    <div className="container-wrapper">
+      <div className="container">
+        <div className="left">
+          <h2 className="heading">Login</h2>
+        </div>
+        <form className="form">
 
-ReactDOM.render(<Login />, document.getElementById('root'));
+          <div className="formGroup">
+            <label htmlFor="username">Username:</label>
+            <input
+              type="text"
+              id="username"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+          </div>
+          <div className="formGroup">
+            <label htmlFor="password">Password:</label>
+            <input
+              type="password"
+              id="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+          <div className="formGroup forgot-password">
+            <div className="label-input">
+              <input
+                type="checkbox"
+                id="rememberMe"
+                checked={rememberMe}
+                onChange={() => setRememberMe(!rememberMe)}
+              />
+              <label htmlFor="rememberMe">Recuérdame</label>
+            </div>
+            <a href="#">¿Olvidaste tu contraseña?</a>
+          </div>
+
+          <button type="button" onClick={handleLogin}>
+            Login
+          </button>
+        </form>
+      </div>
+    </div>
+  </>
+  );
+};
+
+export default Login;
