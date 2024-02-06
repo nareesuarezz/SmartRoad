81d0ca625c25a4f982db8422c36cb36855354140
AdonayXII <joseadonayortegapena@alumno.ieselrincon.es>
2024-02-06 19:26:31 +0000

    issue17

--- frontend/src/pages/car/Car.js
+++ frontend/src/pages/car/Car.js
@@ -12,50 +12,55 @@ function Car() {
     const [subscription, setSubscription] = useState(null);
 
 
-    //Location
+    // Location
     const addTrackGeo = async () => {
         try {
+            // Obtener la ubicación utilizando trackGeo
+            const location = await trackGeo();
+
             const response = await axios.get('http://localhost:8080/api/vehicles');
             const vehicle = response.data;
             const lastVehicleId = vehicle[vehicle.length - 1].UID;
 
-            // const data = {
-            //     Latitude: '',
-            //     Longitude: '',
-            //     Status: 'Stopped',
-            //     Speed: '4',
-            //     Extra: 'pepe',
-            //     Vehicle_UID: lastVehicleId,
-            // }
+            const data = {
+                Latitude: '',
+                Longitude: '',
+                Status: 'Stopped',
+                Speed: '0',
+                Extra: '',
+                Vehicle_UID: lastVehicleId,
+                Location: location, // Usar la ubicación obtenida de trackGeo aquí
+            };
 
-            console.log(data)
-            await axios.post('http://localhost:8080/api/tracks', {
-                ...data,
-                location: trackGeo
-            })
+            // Imprimir la ubicación para verificar
+            // console.log('Ubicación obtenida:', location);
+
+            // Llamar a axios.post con los datos actualizados
+            await axios.post('http://localhost:8080/api/tracks', data);
 
         } catch (err) {
-            console.error(err);
-        }
-    }
-
-    const trackGeo = async () => {
-        try {
-            navigator.geolocation.watchPosition(async (position) => {
-                const location = {
-                    type: 'Point',
-                    coordinates: [position.coords.latitude, position.coords.longitude],
-                };
-
-                console.log("Latitude: " + position.coords.latitude + "\nLongitude: " + position.coords.longitude);
-                return location;
-
-            });
-        } catch (err) {
-            console.error(err);
+            console.error(err.response);
         }
     };
 
+    //Función para recoger la localización y devolverla
+    const trackGeo = () => {
+        return new Promise((resolve, reject) => {
+            try {
+                navigator.geolocation.getCurrentPosition((position) => {
+                    const location = {
+                        type: 'Point',
+                        coordinates: [position.coords.latitude, position.coords.longitude],
+                    };
+                    // console.log("Latitude: " + position.coords.latitude + "\nLongitude: " + position.coords.longitude);
+                    resolve(location);  //Devuelve la localización recogida
+                });
+            } catch (error) {
+                console.error(error);
+                reject(error);
+            }
+        });
+    };
 
     useEffect(() => {
         // Call the function here to start tracking
