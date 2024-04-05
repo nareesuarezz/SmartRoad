a834685b930a7f8137a127ac60242f49205f43b3
nareesuarezz <naremehs11@gmail.com>
2024-04-05 08:25:36 +0100

    funciona xfavor

--- backend/config/db.config.js
+++ backend/config/db.config.js
@@ -12,3 +12,11 @@ module.exports = {
     idle: 10000
   }
 };
+
+sequelize.authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
