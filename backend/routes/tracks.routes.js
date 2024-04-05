e507dd6a54ea828fffba9fa24c5ae664ac43766a
AdonayXII <118464117+AdonayXII@users.noreply.github.com>
2024-04-05 08:22:33 +0100

    ad

--- backend/routes/tracks.routes.js
+++ backend/routes/tracks.routes.js
@@ -30,16 +30,8 @@ module.exports = app => {
   // Nuevo endpoint para calcular la distancia total recorrida por los vehículos de un administrador
   router.get("/distance/admin/:Admin_UID", tracks.calculateTotalDistance);
 
-<<<<<<< HEAD
-  // Retrieve a single Car with type and id
-  router.get("/:type/:id", tracks.timeCar);
-=======
   // Retrieve the total time for the vehicles
   router.get("/totalTime/:Admin_UID", tracks.calculateTotalTime);
->>>>>>> ba287902620bc279447f8129320b2148ff258909
-
-  // Nuevo endpoint para obtener el tiempo del coche
-  router.get("/timeCar/:Vehicle_UID", tracks.timeCar);
 
   app.use("/api/tracks", router);
 }
