d42be36f3981eae22c83923d163a490764b1d12f
AdonayXII <118464117+AdonayXII@users.noreply.github.com>
2024-06-06 08:37:15 +0100

    pas

--- backend/vehicleSimulator.js
+++ backend/vehicleSimulator.js
@@ -2,20 +2,12 @@ const axios = require('axios');
 const fs = require('fs');
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 
+const time = 5;
+
 async function getRoute(start, end) {
   const response = await axios.get(`https://api.openrouteservice.org/v2/directions/driving-car?api_key=5b3ce3597851110001cf6248952c05127b6649cf90550f6f533d1058&start=${start.lng},${start.lat}&end=${end.lng},${end.lat}`);
-  const coordinates = response.data.features[0].geometry.coordinates;
-
-  // Genera puntos aleatorios a lo largo de la ruta
-  const randomPoints = coordinates.map(coord => {
-    const randomLat = coord[1] + Math.random() * 0.01 - 0.005; // Añade o resta hasta 0.005 grados a la latitud
-    const randomLng = coord[0] + Math.random() * 0.01 - 0.005; // Añade o resta hasta 0.005 grados a la longitud
-    return [randomLng, randomLat];
-  });
-
-  return randomPoints;
+  return response.data.features[0].geometry.coordinates;
 }
-
 
 class Vehicle {
   constructor(id, type, route, backendUrl, admin_uid) {
@@ -24,7 +16,6 @@ class Vehicle {
     this.admin_uid = admin_uid;
     this.route = route;
     this.backendUrl = backendUrl;
-    this.speed = Number((Math.random() * 300 + 30).toFixed(2)); // Asigna una velocidad constante al vehículo
   }
 
   async register() {
@@ -32,11 +23,11 @@ class Vehicle {
     return response;
   }
 
-  async sendTrack(coord) {
+  async sendTrack(coord, speed) {
     const track = {
       Location: { type: 'Point', coordinates: [coord[1], coord[0]] },
       Status: 'Moving',
-      Speed: this.speed,
+      Speed: speed,
       Type: 'Simulation',
       Extra: {},
       Vehicle_UID: this.id
@@ -45,29 +36,52 @@ class Vehicle {
     return response.data;
   }
 
+  calculateDistance(coord1, coord2) {
+    const R = 6371e3; // Radio medio de la tierra en metros
+    const lat1 = coord1[1] * Math.PI / 180; // Convertir a radianes
+    const lat2 = coord2[1] * Math.PI / 180;
+    const deltaLat = (coord2[1] - coord1[1]) * Math.PI / 180;
+    const deltaLng = (coord2[0] - coord1[0]) * Math.PI / 180;
+
+    const a = Math.sin(deltaLat / 2) * Math.sin(deltaLat / 2) +
+      Math.cos(lat1) * Math.cos(lat2) *
+      Math.sin(deltaLng / 2) * Math.sin(deltaLng / 2);
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+
+    return R * c; // Distancia en metros
+  }
+
+  calculateSpeed(distance, time) {
+    const speedInMetersPerSecond = distance / time; // Velocidad en m/s
+    const speedInKmH = speedInMetersPerSecond * 3.6;
+    return speedInKmH
+  }
+
   async start() {
     try {
       const registerResponse = await this.register();
       if (registerResponse.status === 200) { // Asegúrate de que el registro fue exitoso
-        let prevCoord = null;
+        let prevCoord;
         for (let coord of this.route) {
           console.log(`Vehicle ${this.id} is at ${coord}`);
-          const trackResponse = await this.sendTrack(coord);
           if (prevCoord) {
-            const distance = Math.sqrt(Math.pow(prevCoord[0] - coord[0], 2) + Math.pow(prevCoord[1] - coord[1], 2)); // Calcula la distancia euclidiana
-            const waitTime = distance / this.speed; // Correlaciona la velocidad con la distancia
-            await new Promise(resolve => setTimeout(resolve, waitTime * 5000)); // Espera un tiempo proporcional a la distancia antes de moverse al siguiente punto
+            const distance = this.calculateDistance(prevCoord, coord);
+            const speed = this.calculateSpeed(distance, time); // 5 segundos entre cada punto
+            await this.sendTrack(coord, speed);
+          } else {
+            await this.sendTrack(coord, 1); // Velocidad inicial es 0
           }
           prevCoord = coord;
+          await new Promise(resolve => setTimeout(resolve, 5000));
         }
       } else {
-        console.log(`Failed to register vehicle ${this.id}. Status code: ${registerResponse.status}`);
+        console.log(registerResponse.status)
+        console.log(`Failed to register vehicle ${this.id}`);
       }
     } catch (error) {
       console.error(`Error starting vehicle ${this.id}:`, error);
     }
   }
-
 }
 
 const pointsA = [
@@ -84,31 +98,30 @@ const pointsA = [
 ];
 
 const pointsB = [
-  { "lat": 27.9847575, "lng": -15.5000564 },
-  { "lat": 27.9884719, "lng": -15.3747598 },
-  { "lat": 28.0840758, "lng": 15.4617254 },
+  { "lat": 28.1151673, "lng": -15.4357038 },
+  { "lat": 28.1327237, "lng": -15.4322041 },
+  { "lat": 28.0947911, "lng": -15.4464587 },
+  { "lat": 28.0859946, "lng": -15.4324199 },
+  { "lat": 28.0979849, "lng": -15.422047 },
+  { "lat": 28.0831454, "lng": -15.442992 },
   { "lat": 28.0992793, "lng": -15.471335 },
-  { "lat": 28.0103162, "lng": -15.5331081 },
-  { "lat": 28.1081819, "lng": -15.4217004 },
-  { "lat": 28.1344405, "lng": -15.433243 },
-  { "lat": 28.1097543, "lng": -15.4165598 },
-  { "lat": 28.1328296, "lng": -15.4350141 },
-  { "lat": 28.1585439, "lng": -15.4111716 }
+  { "lat": 28.1047834, "lng": -15.5635292 },
+  { "lat": 28.0457739, "lng": -15.4776423 },
+  { "lat": 27.9387428, "lng": -15.3896664 }
 ];
 
-async function generateRoutes(startIndex, endIndex) {
+async function generateRoutes() {
   const routes = [];
-  const start = pointsA[startIndex];
-  const end = pointsB[endIndex];
-  const route = await getRoute(start, end);
-  routes.push(route);
+  for (let i = 0; i < Math.min(pointsA.length, pointsB.length, 20); i++) { // Limita la creación de rutas a 20
+    // Genera una ruta desde el puntoA hasta un puntoB
+    const route = await getRoute(pointsA[i], pointsB[i]);
+    routes.push(route);
+  }
   return routes;
 }
 
-// Luego puedes llamar a esta función con los índices de los puntos que quieras usar:
-generateRoutes(0, 9).then(routes => {
+generateRoutes().then(routes => {
   // Crea vehículos y los pone en marcha
   const vehicles = routes.map((route, i) => new Vehicle(i, 'car', route, 'https://localhost'));
   vehicles.forEach(vehicle => vehicle.start());
 });
-
